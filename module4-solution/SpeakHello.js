7df29c066759dcaa8307f5cbc990b4f40bf688ba
Ashley Horne <ashleyhorne348@yahoo.com>
2021-02-15 18:24:00 -0500

    update

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,6 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function(window)){
+(function(start)){
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -21,5 +21,5 @@ helloSpeaker.speak = function (name) {
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
-})(window);
+start.helloSpeaker = helloSpeaker;
+})(start);
